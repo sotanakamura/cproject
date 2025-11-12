7a349875223bdad212fcd654ca67d2c81864f6ad
Sota Nakamura <sota.n_100527@icloud.com>
2025-11-13 00:11:21 +0900

    move the  cursor and use buffer

--- cproject.c
+++ cproject.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
+
+#include <conio.h>
 
 enum status
 {
@@ -20,20 +23,68 @@ struct task
 {
     enum status status;
     int id;
-    char* wbs;
-    char* name;
-    char* desc;
-    char* worker;
+    char wbs[8];
+    char name[64];
+    char worker[16];
     int hours;
     struct tm start;
     struct tm end;
     int level;
     int dep;
     enum rule rule;
-    int manu;  
+    int manu;
 };
+
+void move_cursor(int key, int *row, int *col)
+{
+    switch (key)
+    {
+    case 72:
+        printf("\e[1A");
+
+        break;
+
+    case 75:
+        printf("\e[1D");
+        break;
+
+    case 77:
+        printf("\e[1C");
+        break;
+
+    case 80:
+        printf("\e[1B");
+        break;
+
+    default:
+        break;
+    }
+}
 
 int main()
 {
-    struct task task = {.id = 1};
+    int ch;
+    int row = 0;
+    int col = 0;
+    struct task tasks[1024] = {0};
+    printf("\e[?1049h");
+    printf("\e[0;0H");
+    while (1) {
+        ch = _getch();
+        switch (ch)
+        {
+        case 0:
+        case 0xE0:
+            ch = _getch();
+            move_cursor(ch, &row, &col);
+            break;
+
+        case 'q':
+            printf("\e[?1049l");
+            return 0;
+
+        default:
+            break;
+        }
+    }
 }
